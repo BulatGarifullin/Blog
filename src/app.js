c123af366776e90849b655a965e41c89d4add858
BulatGarifullin <130639959+BulatGarifullin@users.noreply.github.com>
2024-10-26 13:41:05 +0300

    commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
+
+export const App = () => {
+	return (
+		<Div>
+			<i class="fa fa-bluetooth" aria-hidden="true"></i>
+			<div>123</div>
+		</Div>
+	);
+};
